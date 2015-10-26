16ee7a70f25df572d16bcd1447744b25d3dbb256
Yanina Aular (Vauxoo) <yanina.aular@vauxoo.com>
2015-10-26 10:15:03 -0430

    [ADD] Readme description

---
+++ README.rst
@@ -0,0 +1,8 @@
+========================
+Symbolic Link To Project
+========================
+
+
+This script is for create symbolic links to another
+folder that contains a project to work.
+
--- enlaces.py
+++ enlaces.py
@@ -2,23 +2,24 @@ import os
 private = "/home/yanina/vauxoo/private_addons/"
 public = "/home/yanina/vauxoo/public_addons/"
 
-links = [\
-"%sodoo" % public,
-"%spartner-contact" % public,
-"%shr" % public,
-"%sodoo-panama" % public,
-"%sstock-logistics-workflow" % public,
-"%spanama-dv" % public,
-"%swebsite_multi_image" % public,
-"%sproduct-attribute" % public,
-"%sodoo-themes" % private,
-"%sodoo-snippets" % private,
-"%sSerpentCS_Contributions-v8" % private,
-"%sserver-tools" % public,
-"%spartner-contact" % private,
-"%svauxoo_crm" % public,
-"%srma" % public,
-"%syoytec_rebase" % private,]
+links = [
+    "%sodoo" % public,
+    "%spartner-contact" % public,
+    "%shr" % public,
+    "%sodoo-panama" % public,
+    "%sstock-logistics-workflow" % public,
+    "%spanama-dv" % public,
+    "%swebsite_multi_image" % public,
+    "%sproduct-attribute" % public,
+    "%sodoo-themes" % private,
+    "%sodoo-snippets" % private,
+    "%sSerpentCS_Contributions-v8" % private,
+    "%sserver-tools" % public,
+    "%spartner-contact" % private,
+    "%svauxoo_crm" % public,
+    "%srma" % public,
+    "%syoytec_rebase" % private,
+]
 
 for link in links:
-	os.system('ln -s %s .' % link)
+    os.system('ln -s %s .' % link)
